6da00e74b4cd6ab302d8259e0eccac5e9ab6bc81
marudhu paandian <marudhupaandian@gmail.com>
2020-04-02 11:46:45 +0530

    added basic attributes for the characters
    Signed-off-by: marudhu paandian <marudhupaandian@gmail.com>

---
+++ LCCV2/chars.py
@@ -0,0 +1,34 @@
+# Entity class for all characters
+class Entity:
+
+    # Default variables
+    hp = 100
+    facing = None
+    speed = 10
+
+    def __init__(self, x, y, width, height):
+
+        # Getting the character location setup
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    # Taking health away
+    def hurt(self, damage):
+        self.hp -= damage
+
+    # Getting health
+    def re_gen(self, hp):
+        self.hp += hp
+
+
+class Enemy(Entity):
+
+    # Variables
+    points = 100
+    hit = 5
+
+    # Class initialization
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
