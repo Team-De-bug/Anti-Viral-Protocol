b91c5f52f8f98629c45fb19d2d138515a3718663
marudhu paandian <marudhupaandian@gmail.com>
2020-04-02 17:13:23 +0530

    added base weapons class
    Signed-off-by: marudhu paandian <marudhupaandian@gmail.com>

---
+++ LCCV2/items.py
@@ -0,0 +1,23 @@
+class Weapons:
+
+    # Variables
+    ammo = 100
+    damage = 10
+    hold_limit = 25
+    on_load = 25
+
+    # Initializer
+    def __init__(self, on_player):
+        self.on_player = on_player
+
+    # Animation loader
+    @classmethod
+    def load_anim(cls):
+        print("animation not linked")
+
+    # ammo firing function
+    def fire(self):
+        self.ammo -= 1
+
+    def load(self):
+        self.ammo -= self.hold_limit - self.on_load
--- LCCV2/main.py
+++ LCCV2/main.py
@@ -4,7 +4,7 @@ import pygame
 
 # Working file paths
 WORK_FOLDER = os.path.dirname(__file__)
-FINAL_PATH = os.path.join(WORK_FOLDER, 'resources/Images/Icon')
+IMAGES_PATH = os.path.join(WORK_FOLDER, 'resources/Images/Icon')
 
 # Starting pygame
 pygame.init()
@@ -12,7 +12,7 @@ pygame.init()
 # Setting the screen up
 SCREEN = pygame.display.set_mode((800, 640))
 pygame.display.set_caption("LCC GAME")
-ICON = pygame.image.load(os.path.join(FINAL_PATH, 'GameIcon_64.png'))
+ICON = pygame.image.load(os.path.join(IMAGES_PATH, 'GameIcon_64.png'))
 pygame.display.set_icon(ICON)
 
 # Running the game
