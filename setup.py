83777a5849ab0b3adce99a5db09bb6f831f8ef56
Marudhu Paandian K <marudhupaandian@gmail.com>
2021-01-12 19:01:27 +0530

    added build related files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from glob import glob
+
+
+# helper function to get valid files
+def cleaned_list(files):
+    cleaned = []
+    for file in files:
+        if "." in file:
+            cleaned.append(file)
+    return cleaned
+
+
+resources = glob('anti_viral_protocol/resources/**/*', recursive=True)
+setup(
+    name='Anti-Viral-Protocol',
+    version='1.0.1',
+    packages=['anti_viral_protocol'],
+    license_file='LICENSE',
+    url='https://github.com/Team-De-bug/Anti-Viral-Protocol',
+    author='marudhu',
+    author_email='marudhupaandian@gmail.com',
+    description='2d platformer, side scroller game',
+    install_requires='pygame',
+    include_package_data=True,
+    data_files=[('resources', cleaned_list(resources))]
+)
